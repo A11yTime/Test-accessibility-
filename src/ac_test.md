800179997d30d6a75948768e52a02e8538a68fbc
aciosui <mrana.swe@gmail.com>
2024-12-08 10:38:59 -0500

    added accessibility test

---
+++ src/ac_test.md
@@ -0,0 +1,3 @@
+just run this command for accessibility test:
+
+npm install --save-dev jest @testing-library/vue jest-axe @vue/test-utils
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,32 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import { axe, toHaveNoViolations } from 'jest-axe';
+import { render } from '@testing-library/vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+expect.extend(toHaveNoViolations);
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+  it('should have no accessibility violations', async () => {
+    const { container } = render(HelloWorld);
+    const results = await axe(container, {
+      rules: {
+        'color-contrast': { enabled: false },  // Disabling a specific rule, as an example
+      },
+    });
+    expect(results).toHaveNoViolations();
+  });
+});
+
+
+// import { shallowMount } from '@vue/test-utils'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       props: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
